4357e3d493777869f5fb39aaa3e14d2124404498
handuah <handuah@gmail.com>
2022-08-09 13:09:31 +0000

    created pages up to fourth page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/second_page'
+  get 'home/third_page'
+  get 'home/fourth_page'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
