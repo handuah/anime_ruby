c7d3833c0ab5753da070a14cfd1590e2d440d6bb
handuah <handuah@gmail.com>
2022-08-09 12:15:25 +0000

    added a second page path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @all_anime = AnimeList.new.first_page_json["data"]
+    
+  end
+
+  def second_page
+    @all_anime2 = AnimeList.new.second_page_json["data"]
   end
 end
