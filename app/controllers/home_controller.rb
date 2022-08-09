35d9ccc832855290e4e4b315ef5c3ea1104c9c9d
handuah <handuah@gmail.com>
2022-08-09 13:08:20 +0000

    created third and fourth pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,17 @@
 class HomeController < ApplicationController
   def index
-    @all_anime = AnimeList.new.first_page_json["data"]
-    
+    @all_anime = AnimeList.new.first_page_json["data"]    
   end
 
   def second_page
     @all_anime2 = AnimeList.new.second_page_json["data"]
+  end
+
+  def third_page
+    @all_anime3 = AnimeList.new.third_page_json["data"]
+  end
+
+  def fourth_page
+    @all_anime4 = AnimeList.new.fourth_page_json["data"]
   end
 end
