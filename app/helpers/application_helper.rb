8b67dfa7e8f9fe18818f9d0f06868583a491827a
handuah <handuah@gmail.com>
2023-03-02 12:05:12 +0000

    added helper for the title of pages in application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def main_title(separator = " | ")
+        [content_for(:title), 'AnimeRuby'].compact.join(separator)
+      end
+      
+      def selected_anime_page_title(title)
+        content_for(:title){ title }
+        content_tag(:h1, title)
+      end
 end
